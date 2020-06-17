7049dd32eec130b9e1da9cb7f71d2b30d388365a
Stu Hood <stuhood@gmail.com>
2020-06-17 14:18:15 -0700

    WIP: Begin migrating build. No pythonpath included/embedded yet.
    [ci skip-jvm-tests]

--- src/rust/engine/src/main.rs
+++ src/rust/engine/src/main.rs
@@ -43,9 +43,42 @@ mod selectors;
 mod tasks;
 mod types;
 
+// TODO: This is no longer an importable module, so these `pub use` statements should be
+// removed/inlined-into-consumers-within-this-crate.
 pub use crate::context::Core;
 pub use crate::core::{Failure, Function, Key, Params, TypeId, Value};
 pub use crate::intrinsics::Intrinsics;
 pub use crate::scheduler::{ExecutionRequest, ExecutionTermination, Scheduler, Session};
 pub use crate::tasks::{Rule, Tasks};
 pub use crate::types::Types;
+
+// Include the pyoxidizer config created by `build.rs`.
+include!(env!("PYOXIDIZER_DEFAULT_PYTHON_CONFIG_RS"));
+
+pub fn main() {
+    // The following code is in a block so the MainPythonInterpreter is destroyed in an
+    // orderly manner, before process exit.
+    let code = {
+        // Load the configuration defined by the include! above.
+        let config = default_python_config();
+
+        // Construct a new Python interpreter using that config, handling any errors
+        // from construction.
+        match pyembed::MainPythonInterpreter::new(config) {
+            Ok(mut interp) => {
+                // And run it using the default run configuration as specified by the
+                // configuration. If an uncaught Python exception is raised, handle it.
+                // This includes the special SystemExit, which is a request to terminate the
+                // process.
+                interp.run_as_main()
+            }
+            Err(msg) => {
+                eprintln!("{}", msg);
+                1
+            }
+        }
+    };
+
+    // And exit the process according to code execution results.
+    std::process::exit(code);
+}
